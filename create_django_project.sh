3507926ac6d68a8ad3077642fb9e0256aeb5cc5f
bakmenson <bakmenson@gmail.com>
2020-02-24 23:30:21 +0300

    added check if pyenv virtualenv and python version exists

--- create_django_project.sh
+++ create_django_project.sh
@@ -4,7 +4,12 @@ project_dir=$1
 virtual_env=$2
 python_version=$3
 
-PATTERN="^[a-z-]+"
+VIRTUAL_ENV_PATTERN="^[a-z-]+"
+
+#PYTHON_VERSION_PATTERN="^..\d.\d.\d$"
+PYTHON_VERSION_PATTERN="^\w.\w.\w$"
+
+cd ../
 
 if [[ -d $project_dir ]]; then
 	rm -rf $project_dir
@@ -14,11 +19,17 @@ mkdir $project_dir
 cd $project_dir
 
 while read -r line; do
-	if [[ $line =~ ${PATTERN} ]]; then
-
+	if [[ $line =~ ${VIRTUAL_ENV_PATTERN} ]]; then
 		if [[ $virtual_env =~ ${BASH_REMATCH} ]]; then
 			pyenv local $virtual_env
 		fi
-
 	fi
 done < <(pyenv virtualenvs)
+
+while read -r line; do
+	if [[ $line =~ ${PYTHON_VERSION_PATTERN} ]]; then
+		if [[ $python_version =~ ${BASH_REMATCH} ]]; then
+			echo $python_version
+		fi
+	fi
+done < <(pyenv versions)
