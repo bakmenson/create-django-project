61684ca5ebdfc60d49a6514754802d91297b66ea
bakmenson <bakmenson@gmail.com>
2020-02-25 23:38:05 +0300

    added check if args python version and env not is empty

--- create_django_project.sh
+++ create_django_project.sh
@@ -1,35 +1,51 @@
 #!/bin/bash
 
-project_dir=$1
-virtual_env=$2
-python_version=$3
+#project_dir=$1
+virtual_env=$1
+python_version=$2
 
 VIRTUAL_ENV_PATTERN="^[a-z-]+"
 
-#PYTHON_VERSION_PATTERN="^..\d.\d.\d$"
-PYTHON_VERSION_PATTERN="^\w.\w.\w$"
+is_available_python_version=false
+is_available_virtual_env=false
 
-cd ../
+if [[ $python_version != "" ]]; then
+	while read -r line; do
+		if [[ $line == $python_version ]]; then
+			is_available_python_version=true
+		fi
+	done < <(pyenv versions)
 
-if [[ -d $project_dir ]]; then
-	rm -rf $project_dir
+	if ! $is_available_python_version; then
+		pyenv install $python_version
+		is_available_python_version=true
+	fi
 fi
 
-mkdir $project_dir
-cd $project_dir
+#cd ../
 
-while read -r line; do
-	if [[ $line =~ ${VIRTUAL_ENV_PATTERN} ]]; then
-		if [[ $virtual_env =~ ${BASH_REMATCH} ]]; then
-			pyenv local $virtual_env
-		fi
-	fi
-done < <(pyenv virtualenvs)
+#if [[ -d $project_dir ]]; then
+#	rm -rf $project_dir
+#fi
 
-while read -r line; do
-	if [[ $line =~ ${PYTHON_VERSION_PATTERN} ]]; then
-		if [[ $python_version =~ ${BASH_REMATCH} ]]; then
-			echo $python_version
+#mkdir $project_dir
+#cd $project_dir
+
+if [[ $virtual_env != "" ]]; then
+	is_available_virtual_env=true
+	while read -r line; do
+		if [[ $line =~ ${VIRTUAL_ENV_PATTERN} ]]; then
+			if [[ $virtual_env =~ ${BASH_REMATCH} ]]; then
+				#pyenv local $virtual_env
+				#exit 0
+				echo $virtual_env
+			fi
 		fi
-	fi
-done < <(pyenv versions)
+	done < <(pyenv virtualenvs)
+fi
+
+if $is_available_python_version && $is_available_virtual_env; then
+	echo $is_available_python_version
+	echo $is_available_virtual_env
+	#pyenv virtualenv $python_version $virtual_env
+fi
