9bae50952a4224576a036934db458036c2ae65d3
bakmenson <bakmenson@gmail.com>
2020-03-05 23:36:43 +0300

    added script for calling create or delete script

---
+++ django_project.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+action=$1
+
+if [[ $action == "-c" ]]; then
+
+	echo_message "Input project dir." 30 && read create_dir
+	echo_message "Input virtua env." 30 && read virtual_env
+	echo_message "Input Python version." 30 && read python_version
+
+	chmod +x create_project.sh
+
+	./create_project.sh $create_dir $virtual_env $pyenv_version
+
+elif [[ $action == "-d" ]]; then
+
+	echo_message "Input project dir." 30 && read remove_project_dir
+
+	chmod +x delete_project.sh
+
+	./delete_project.sh $remove_project_dir
+
+else
+	echo_message "Wrong command." 20 && exit 1
+fi
