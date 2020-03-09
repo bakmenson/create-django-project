e7f87d8aacdaaa133faa65ac5af4b4f2a4ae8e24
bakmenson <bakmenson@gmail.com>
2020-03-09 21:19:52 +0300

    changed get_pyenv_output: remove re pattern

--- django_project.py
+++ django_project.py
@@ -7,9 +7,7 @@ def print_message(message: str) -> None:
     print(f"{'-' * len(message)}\n{message}\n{'-' * len(message)}")
 
 
-def get_pyenv_output(command: str, pattern: str) -> List[str]:
-    output_list: List[str] = []
-
+def get_pyenv_output(command: str) -> List[str]:
     console_output: Tuple[Union[str, bytes], Union[str, bytes]] = Popen(
         [command], shell=True, stdout=PIPE, encoding='utf-8'
     ).communicate()
@@ -18,22 +16,17 @@ def get_pyenv_output(command: str, pattern: str) -> List[str]:
         console_output[0]
     ).replace('\n', '').replace('*', ' ').strip()
 
-    for string in console_output_string.split('  '):
-        match = re.findall(pattern, string)
-        if not match:
-            if ' ' in string:
-                string = string[0:string.index(' ')]
-            output_list.append(string)
+    output_list: List[str] = console_output_string.split('  ')
 
     return output_list
 
 
-VIRTUALENV_PATTERN = r"^(?P<env>[^//])..created.+versions.(?P<version>.+).$"
+VIRTUALENV_PATTERN = r"^(?P<env>[0-9a-zA-z-.]+)..created.+versions.(?P<version>.+).$"
 EXCLUDE_VERSION_PATTERN = r"[0-9a-zA-Z.]+/.+/.+"
 EXCLUDE_VIRTUALENV_PATTERN = r"^.+/envs/"
 
-versions = get_pyenv_output('pyenv versions', EXCLUDE_VERSION_PATTERN)
-virtualenvs = get_pyenv_output('pyenv virtualenvs', EXCLUDE_VIRTUALENV_PATTERN)
+versions = get_pyenv_output('pyenv versions')
+virtualenvs = get_pyenv_output('pyenv virtualenvs')
 
 print(versions)
 print(virtualenvs)
