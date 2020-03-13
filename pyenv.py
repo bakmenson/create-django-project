bd496f8643f0aac9d468c8ce2549eaa9e40f7b29
bakmenson <bakmenson@gmail.com>
2020-03-13 21:32:40 +0300

    removed class Pyenv

--- pyenv.py
+++ pyenv.py
@@ -3,58 +3,54 @@ from re import findall
 from typing import List, Tuple, Union
 
 
-class Pyenv:
-    def __init__(self, env_name, python_version):
-        self._env_name: str = env_name
-        self._python_version: str = python_version
+def get_console_output(command) -> List[str]:
+    console_output: Tuple[Union[str, bytes], Union[str, bytes]] = Popen(
+        [command], shell=True, stdout=PIPE, encoding='utf-8'
+    ).communicate()
 
-    @staticmethod
-    def _get_console_output(command) -> List[str]:
-        console_output: Tuple[Union[str, bytes], Union[str, bytes]] = Popen(
-            [command], shell=True, stdout=PIPE, encoding='utf-8'
-        ).communicate()
+    console_output_string: str = str(
+        console_output[0]
+    ).replace('\n', '').replace('*', ' ').strip()
 
-        console_output_string: str = str(
-            console_output[0]
-        ).replace('\n', '').replace('*', ' ').strip()
+    output_list: List[str] = console_output_string.split('  ')
 
-        output_list: List[str] = console_output_string.split('  ')
+    return output_list
 
-        return output_list
 
-    @staticmethod
-    def _get_regex_console_output(
-            pyenv_output: List[str], regex_pattern: str
-    ) -> List[Union[str, Tuple[str]]]:
+def get_regex_console_output(
+        pyenv_output: List[str], regex_pattern: str
+) -> List[Union[str, Tuple[str]]]:
 
-        result: List[Union[str, Tuple[str]]] = []
+    result: List[Union[str, Tuple[str]]] = []
 
-        for line in pyenv_output:
-            pattern_match = findall(regex_pattern, line)
-            if pattern_match:
-                result.append(*pattern_match)
+    for line in pyenv_output:
+        pattern_match = findall(regex_pattern, line)
+        if pattern_match:
+            result.append(*pattern_match)
 
-        for string in result:
-            if isinstance(string, str):
-                result[result.index(string)] = string.rstrip()
+    for string in result:
+        if isinstance(string, str):
+            result[result.index(string)] = string.rstrip()
 
-        return result
+    return result
 
-    def get_command_output(
-            self, command: str, regex_pattern: str
-    ) -> List[Union[str, Tuple[str]]]:
-        return self._get_regex_console_output(
-            self._get_console_output(command),
-            regex_pattern
-        )
 
-    def install_python(self) -> None:
-        call("pyenv install " + self._python_version, shell=True)
+def get_command_output(
+        command: str, regex_pattern: str
+) -> List[Union[str, Tuple[str]]]:
+    return get_regex_console_output(
+        get_console_output(command),
+        regex_pattern
+    )
 
-    def create_virtualenv(self) -> None:
-        call("pyenv virtualenv " \
-             + self._python_version + " " \
-             + self._env_name, shell=True)
 
-    def set_virtualenv(self) -> None:
-        call("pyenv local " + self._env_name, shell=True)
+def install_python(python_version) -> None:
+    call("pyenv install " + python_version, shell=True)
+
+
+def create_virtualenv(python_version, env_name) -> None:
+    call("pyenv virtualenv " + python_version + " " + env_name, shell=True)
+
+
+def set_virtualenv(env_name) -> None:
+    call("pyenv local " + env_name, shell=True)
