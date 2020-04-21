66c22b1265f5d90bac868766749d0a8c4b3e8aa9
bakmenson <bakmenson@gmail.com>
2020-04-21 21:23:08 +0300

    added class Project

---
+++ project.py
@@ -0,0 +1,9 @@
+class Project:
+    def __init__(self, project_name: str):
+        self._project_name = project_name
+
+    def create_project(self) -> None:
+        pass
+
+    def delete_project(self) -> None:
+        pass
