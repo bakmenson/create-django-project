8a810aa57fa65c013aaaa5652fb5091e7c57dd61
bakmenson <bakmenson@gmail.com>
2020-05-08 10:32:57 +0300

    remove class PyenvAction

--- pyenv.py
+++ pyenv.py
@@ -2,37 +2,26 @@ from subprocess import call, Popen, PIPE
 from re import findall
 
 
-class PyenvAction:
-    def __init__(self, virtualenv_name: str, python_version: str = ""):
-        """
-        :param virtualenv_name: virtualenv name for project
-        :param python_version: python version for project
-        """
-        self._virtualenv_name = virtualenv_name
-        self._python_version = python_version
+def install_python(python_version: str) -> None:
+    """
+    install python version
+    """
+    call("pyenv install " + python_version, shell=True)
 
-    def install_python(self) -> None:
-        """
-        install python version
-        """
-        call("pyenv install " + self._python_version, shell=True)
 
-    def create_virtualenv(self) -> None:
-        """
-        create new virtualenv
-        """
-        call("pyenv virtualenv " + self._python_version + " "
-             + self._virtualenv_name, shell=True)
+def create_virtualenv(python_version: str, virtualenv_name: str) -> None:
+    """
+    create new virtualenv
+    """
+    call("pyenv virtualenv " + python_version + " "
+         + virtualenv_name, shell=True)
 
-    def set_virtualenv(self) -> None:
-        """
-        set virtualenv for project dir
-        """
-        call("pyenv local " + self._virtualenv_name, shell=True)
 
-    def __str__(self):
-        return f"Virtualenv name: {self._virtualenv_name}," \
-               f" Python version: {self._python_version}."
+def set_virtualenv(virtualenv_name: str) -> None:
+    """
+    set virtualenv for project dir
+    """
+    call("pyenv local " + virtualenv_name, shell=True)
 
 
 def pyenv_output(command: str, regex_pattern: str = "") -> list:
