487c25c097d857d4615f6eac9be705a06958044d
bakmenson <bakmenson@gmail.com>
2020-05-08 15:55:42 +0300

    edited vars:

--- django_project.sh
+++ django_project.sh
@@ -6,7 +6,7 @@ echo_message() { separator $2 && echo -e $1 && separator $2; }
 
 action=$1
 
-if [[ $action == "-c" ]]; then
+if [[ ${action} == "-c" ]]; then
 
 	echo_message "Input project dir." 30 && read create_dir
 	echo_message "Input virtua env." 30 && read virtual_env
@@ -14,15 +14,15 @@ if [[ $action == "-c" ]]; then
 
 	chmod +x create_project.sh
 
-	./create_project.sh $create_dir $virtual_env $python_version
+	./create_project.sh ${create_dir} ${virtual_env} ${python_version}
 
-elif [[ $action == "-d" ]]; then
+elif [[ ${action} == "-d" ]]; then
 
 	echo_message "Input project dir." 30 && read remove_project_dir
 
 	chmod +x delete_project.sh
 
-	./delete_project.sh $remove_project_dir
+	./delete_project.sh ${remove_project_dir}
 
 else
 	echo_message "Wrong command." 20 && exit 1
